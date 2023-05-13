dd3206b5d404d3227c4217f318ce3177b2c88d1b
Gourigautam <95241948+Gourigautam@users.noreply.github.com>
2023-05-14 00:13:13 +0530

    Create Duryodhan.java

---
+++ LE_3/LE3_prob1/Duryodhan.java
@@ -0,0 +1,9 @@
+class Duryodhan extends Kaurav{
+    void notorious(){
+        System.out.println("duryodhan were disobedient");
+ }
+    void obey()
+ {
+        System.out.println("disobedience duryodhan");
+ }
+}
