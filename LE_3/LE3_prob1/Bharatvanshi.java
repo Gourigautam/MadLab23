81587f407396e355bff0ebaae26e6fa098eb8990
Gourigautam <95241948+Gourigautam@users.noreply.github.com>
2023-05-14 00:12:04 +0530

    Create Bharatvanshi.java

---
+++ LE_3/LE3_prob1/Bharatvanshi.java
@@ -0,0 +1,9 @@
+abstract class Bharatvanshi
+{
+    void fighter(){
+        System.out.println(" bharatvanshi figter");
+ }
+    abstract void kind();
+    abstract void obey();
+    abstract void notorious();
+}
