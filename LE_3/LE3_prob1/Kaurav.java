c6c53ab817ddbf8a0ca83f52410e822dcba61be3
Gourigautam <95241948+Gourigautam@users.noreply.github.com>
2023-05-14 00:13:42 +0530

    Create Kaurav.java

---
+++ LE_3/LE3_prob1/Kaurav.java
@@ -0,0 +1,14 @@
+class Kaurav extends Bharatvanshi{
+    void notorious()
+ {
+        System.out.println("notorious kauravas");
+ }
+    void obey()
+ {
+         System.out.println("disobedience kauravas");
+ }
+    void kind()
+ {
+         System.out.println(" not kind");
+ }
+}
