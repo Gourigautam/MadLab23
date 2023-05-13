54470ccf51d853c53b4160d434f2372008326df8
Gourigautam <95241948+Gourigautam@users.noreply.github.com>
2023-05-14 00:15:09 +0530

    Create Pandavas.java

---
+++ LE_3/LE3_prob1/Pandavas.java
@@ -0,0 +1,13 @@
+class Pandavs extends Bharatvanshi{
+    void obey(){
+        System.out.println("pandavas obedience");
+ }
+    void kind()
+ {
+        System.out.println("pandavas kind");
+ }
+    void notorious()
+ {
+        System.out.println("not notorious");
+ }
+}
