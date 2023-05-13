81d125d0da9407250c4f272d1391a017b88da132
Gourigautam <95241948+Gourigautam@users.noreply.github.com>
2023-05-14 00:14:25 +0530

    Create main.java

---
+++ LE_3/LE3_prob1/main.java
@@ -0,0 +1,23 @@
+public class Main
+{
+public static void main(String[] args) {
+    Arjun a=new Arjun();
+    a.fighter();
+    a.obey();
+    a.kind();
+    Bheem b=new Bheem();
+    b.fighter();
+    b.obey();
+    b.kind();
+    Vikarn v=new Vikarn();
+    v.fighter();
+    v.obey();
+    v.kind();
+    Pandavs p=new Pandavs();
+    p.kind();
+    p.obey();
+    Duryodhan d=new Duryodhan();
+    d.notorious();
+    d.fighter();
+}
+}
