0994a9eaebfc615123d8c8c6b88dfd62a2a8800e
Gourigautam <95241948+Gourigautam@users.noreply.github.com>
2023-05-14 00:17:27 +0530

    Create LE3_prob2.java

---
+++ LE_3/LE3_prob2.java
@@ -0,0 +1,18 @@
+public class Main{
+    public static void main(String[] args) {
+    Test t=new Test();
+    t.display();
+}
+}
+
+class Test implements Testable{
+    public display(){
+        System.out.println("hello");
+    }
+}
+
+
+Interface Testable{
+    void display();
+    
+}
